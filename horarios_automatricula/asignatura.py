9e42cf2073983fee8ab98a76cf43b97eb07e2c09
juanmaaf <juanmanuelaneasfranco@gmail.com>
2024-11-05 23:56:31 +0100

    closes #8. Defino la clase asignatura basándome en la infomración extraída de HU1, modelando e problema que propone el usuario, donde reflejo los horarios de teoría y prácticas

---
+++ horarios_automatricula/asignatura.py
@@ -0,0 +1,28 @@
+from .horario import Horario
+
+class Asignatura:
+    """
+    Clase que representa una asignatura con un horario de teoría y varios subgrupos de prácticas.
+    """
+    def __init__(self, nombre, horario_teoria: Horario, horarios_practicas, grupo):
+        """
+        Inicializa la asignatura con nombre, horario de teoría, horarios de prácticas, grupo y mención.
+        :param nombre: Nombre de la asignatura.
+        :param horario_teoria: Horario de la clase de teoría.
+        :param horarios_practicas: Lista de horarios de prácticas.
+        :param grupo: El grupo de la asignatura.
+        :param mencion: Tipo de mención, con valor predeterminado BASICA.
+        """
+        if not isinstance(horario_teoria, Horario):
+            raise TypeError("El horario debe instancia de la clase Horario")
+        if not isinstance(horarios_practicas, list):
+            raise TypeError("Los horarios de prácticas deben ser un vector (lista)")
+        
+        for horario_p in horarios_practicas:
+            if not isinstance(horario_p, Horario):
+                raise TypeError("Cada horario de prácticas debe instancia de la clase Horario")
+        
+        self.nombre = nombre
+        self.horario_teoria = horario_teoria
+        self.grupo = grupo
+        self.horarios_practicas = horarios_practicas
