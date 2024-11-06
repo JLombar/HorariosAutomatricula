3616a109567e5eccba5228ed33e4f7cde8995c9d
juanmaaf <juanmanuelaneasfranco@gmail.com>
2024-11-06 21:32:43 +0100

    closes #8. Elimino comentarios clase asignatura para seguir correctamente la metodología. Únicamente basta con la información de los mensajes commit

--- horarios_automatricula/asignatura.py
+++ horarios_automatricula/asignatura.py
@@ -1,16 +1,7 @@
 from .horario import Horario
 
 class Asignatura:
-    """
-    Clase que representa una asignatura con un horario.
-    """
     def __init__(self, nombre, horario: Horario, grupo):
-        """
-        Inicializa la asignatura con nombre, horario de teoría, horarios de prácticas, grupo y mención.
-        :param nombre: Nombre de la asignatura.
-        :param horario: Horario de la asignatura.
-        :param grupo: El grupo de la asignatura.
-        """
         if not isinstance(horario, Horario):
             raise TypeError("El horario debe instancia de la clase Horario")
         
