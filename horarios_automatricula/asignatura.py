f9be3cab966fcff6e15a581c981d0af6c0f58fcf
juanmaaf <juanmanuelaneasfranco@gmail.com>
2024-11-06 21:13:02 +0100

    closes #8. Elimino información de horarios de prácticas. No produce un producto mínimo que resuelve el problema definido en la HU. Sigo la metodología y las Buenas Prácticas.

--- horarios_automatricula/asignatura.py
+++ horarios_automatricula/asignatura.py
@@ -2,26 +2,18 @@ from .horario import Horario
 
 class Asignatura:
     """
-    Clase que representa una asignatura con un horario de teoría y varios subgrupos de prácticas.
+    Clase que representa una asignatura con un horario.
     """
-    def __init__(self, nombre, horario_teoria: Horario, horarios_practicas, grupo):
+    def __init__(self, nombre, horario: Horario, grupo):
         """
         Inicializa la asignatura con nombre, horario de teoría, horarios de prácticas, grupo y mención.
         :param nombre: Nombre de la asignatura.
-        :param horario_teoria: Horario de la clase de teoría.
-        :param horarios_practicas: Lista de horarios de prácticas.
+        :param horario: Horario de la asignatura.
         :param grupo: El grupo de la asignatura.
         """
-        if not isinstance(horario_teoria, Horario):
+        if not isinstance(horario, Horario):
             raise TypeError("El horario debe instancia de la clase Horario")
-        if not isinstance(horarios_practicas, list):
-            raise TypeError("Los horarios de prácticas deben ser un vector (lista)")
-        
-        for horario_p in horarios_practicas:
-            if not isinstance(horario_p, Horario):
-                raise TypeError("Cada horario de prácticas debe instancia de la clase Horario")
         
         self.nombre = nombre
-        self.horario_teoria = horario_teoria
+        self.horario = horario
         self.grupo = grupo
-        self.horarios_practicas = horarios_practicas
