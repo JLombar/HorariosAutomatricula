7d605382bc6a8c1abc250264d1a31d1a02a6d0cb
juanmaaf <juanmanuelaneasfranco@gmail.com>
2024-11-07 12:50:44 +0100

    closes #8. Añado a asignatura la verificación de la validez del parámetro grupos, indicando que cada grupo debe ser instancia de la clase Grupo

--- horarios_automatricula/asignatura.py
+++ horarios_automatricula/asignatura.py
@@ -1,4 +1,10 @@
+from .grupo import Grupo
+
 class Asignatura:
     def __init__(self, nombre, grupos):
+        for grupo in grupos:
+            if not isinstance(grupo, Grupo):
+                raise TypeError("Cada grupo debe instancia de la clase Grupo")
+            
         self.nombre = nombre
         self.grupos = grupos
