126d71623199886ae5f6f4cde1987ed450e34e86
juanmaaf <juanmanuelaneasfranco@gmail.com>
2024-11-07 12:54:22 +0100

    closes #12. Sustituyo nombre por letra en los atributos ya que tiene más sentido al hablar de un grupo

--- horarios_automatricula/grupo.py
+++ horarios_automatricula/grupo.py
@@ -1,9 +1,9 @@
 from .horario import Horario
 
 class Grupo:
-    def __init__(self, nombre, horario: Horario):
+    def __init__(self, letra, horario: Horario):
         if not isinstance(horario, Horario):
             raise TypeError("El horario debe instancia de la clase Horario")
         
-        self.nombre = nombre
+        self.letra = letra
         self.horario = horario
