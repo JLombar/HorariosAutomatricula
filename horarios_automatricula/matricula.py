15412e27f66d5d213945f1b250fcacf8354a6c2c
Jorge Lombardo <jorge.lombardo.8@gmail.com>
2024-11-08 19:30:56 +0100

    closes #14 mejorado código de objetivos anteriores para seguir las recomendaciones de PEP 8

--- horarios_automatricula/matricula.py
+++ horarios_automatricula/matricula.py
@@ -1,9 +1,13 @@
 from .asignatura import Asignatura_Grupos
 
+
 class Matricula:
     def __init__(self, asignaturas):
         for asignatura in asignaturas:
             if not isinstance(asignatura, Asignatura_Grupos):
-                raise TypeError("Cada asignatura debe instancia de la clase Asignatura_Grupos")
-            
+                raise TypeError(
+                    "Cada asignatura debe ser una instancia de la clase "
+                    "Asignatura_Grupos"
+                )
+
         self.asignaturas = asignaturas
