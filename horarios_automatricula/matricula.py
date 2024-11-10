da1f6945b4c4947c647b8181d3ad1bafc6ac31c3
Jorge Lombardo <93705662+JLombar@users.noreply.github.com>
2024-11-10 10:28:24 +0100

    closes #17 arreglando las sintaxis errónea de la clase matrícula

--- horarios_automatricula/matricula.py
+++ horarios_automatricula/matricula.py
@@ -4,10 +4,10 @@ from .asignatura import AsignaturaGrupos
 class Matricula:
     def __init__(self, asignaturas):
         for asignatura in asignaturas:
-            if not isinstance(asignatura, Asignatura_Grupos):
+            if not isinstance(asignatura, AsignaturaGrupos):
                 raise TypeError(
                     "Cada asignatura debe ser una instancia de la clase "
-                    "Asignatura_Grupos"
+                    "AsignaturaGrupos"
                 )
 
         self.asignaturas = asignaturas
