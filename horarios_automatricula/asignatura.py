1f4f32b5fcd0a1c9c36166c52d822e87b8a72f91
Jorge Lombardo <jorge.lombardo.8@gmail.com>
2024-11-21 18:18:18 +0100

    closes #21 creando la funcón que leera el archivo de las asignaturas

--- horarios_automatricula/asignatura.py
+++ horarios_automatricula/asignatura.py
@@ -9,14 +9,14 @@ class Asignatura_Grupos:
         self.nombre = nombre
         self.grupos = grupos
 
-def leer_asignaturas():
+def leer_asignaturas(archivo):
     asignaturas = []
     try:
-        with open('../docs/asignaturas.txt', 'r', encoding='utf-8') as f:
+        with open(archivo, 'r', encoding='utf-8') as f:
             for linea in f:
                 asignaturas.append(linea.strip())
         return asignaturas
     except FileNotFoundError:
-        print("No se ha encontrado el archivo 'asignaturas.txt' en la ruta correcta.")
+        print("No se ha encontrado el archivo en la ruta correcta.")
     except Exception as e:
         print(f"Error al leer el archivo: {e}")
