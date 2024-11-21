b253c83e9febbf8c1876c358aa83944edd6a3580
Jorge Lombardo <jorge.lombardo.8@gmail.com>
2024-11-21 17:15:26 +0100

    closes #21, añadiendo función en asignaturas que lee el archivo txt

--- horarios_automatricula/asignatura.py
+++ horarios_automatricula/asignatura.py
@@ -8,3 +8,15 @@ class Asignatura_Grupos:
             
         self.nombre = nombre
         self.grupos = grupos
+
+def leer_asignaturas():
+    asignaturas = []
+    try:
+        with open('../docs/asignaturas.txt', 'r', encoding='utf-8') as f:
+            for linea in f:
+                asignaturas.append(linea.strip())
+        return asignaturas
+    except FileNotFoundError:
+        print("No se ha encontrado el archivo 'asignaturas.txt' en la ruta correcta.")
+    except Exception as e:
+        print(f"Error al leer el archivo: {e}")
