b631b59894028e10d2d7fa483111ba49c4eb3cc5
Jorge Lombardo <jorge.lombardo@nazaries.com>
2024-11-27 20:39:03 +0100

    closes #18 cambiando la clase grupo para que funcione con los tests

--- horarios_automatricula/grupo.py
+++ horarios_automatricula/grupo.py
@@ -1,9 +1,8 @@
 from .horario import Horario
 
 class Grupo:
-    def __init__(self, letra, horario):
-        if not isinstance(horario, Horario):
-            raise TypeError("El horario debe ser una instancia de la clase Horario")
-        
+    def __init__(self, letra, horarios: Horario):
+        if not (isinstance(horarios, list) and all(isinstance(h, Horario) for h in horarios)):
+            raise TypeError("El horario debe ser una lista de instancias de la clase Horario")
         self.letra = letra
         self.horarios = horarios
